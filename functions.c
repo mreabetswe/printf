50575981d637a4fffa1d7c8425ed15d45e206804
amandamaziba <amandamaziba@gmail.com>
2023-05-30 10:08:36 -0700

    updated

--- functions.c
+++ functions.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
-
 /**
  * print_char - Prints out a character
  * @types: List a of arguments
@@ -19,7 +17,6 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -31,7 +28,7 @@ int print_char(va_list types, char buffer[],
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int length = 0, i;
 	char *str = va_arg(types, char *);
@@ -74,7 +71,6 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -96,8 +92,6 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(size);
 	return (write(1, "%%", 1));
 }
-
-/************************* PRINT INT *************************/
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -140,9 +134,6 @@ int print_int(va_list types, char buffer[],
 
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
-
-/* PRINT BINARY*/
-
 /**
  * print_binary - Prints an unsigned number
  * @types: kinds of arguments
@@ -153,7 +144,8 @@ int print_int(va_list types, char buffer[],
  * @size: amount specifier
  * Return: Numbers of char printed.
  */
-int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_binary(va_list types, char buffer[], int flags, int width,
+		int precision, int size)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
